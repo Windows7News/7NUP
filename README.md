c10d79da2a2a5899a7e5d948c9125aaedfa1041e
faceair <gb285714285>
2014-02-23 00:00:03 +0800

    start

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-## RPUP
+## 7NUP
 
-一个只有单个页面的简单文件分享应用。
+单页面的基于七牛云存储的简单文件分享应用。
+基于[RPUP](https://github.com/jysperm/RPUP)和[simple-qiniu-sdk](https://github.com/zither/simple-qiniu-sdk)构建。
 
-## 运行环境
+## 特点
 
-可运行于 SAE 和普通的 PHP 虚拟主机，前者会使用Storage(默认Domian`rpup`)来储存，后者默认储存于 `files` 文件夹。  
-你需要配置 Web 服务器重写所有请求到 `index.php`, 上面已经包括了常见 Web 服务器的重写规则。
+* 七牛CDN全网加速
+* 支持多媒体在线处理，可按需[生成缩略图](http://developer.qiniu.com/docs/v6/api/reference/fop/image/imageview2.html)等
+
+## 演示地址
+
+[http://7nup.sinaapp.com/](http://7nup.sinaapp.com/)
+
+## 授权
+
+[GPLv3](https://github.com/faceair/7NUP/blob/master/index.php#L8)
--- index.php
+++ index.php
@@ -1,61 +1,18 @@
 <?php
-/*
-精英王子, m@jybox.net
-http://jyprince.me/
-2013.7.28
-
-https://github.com/jybox/RPUP
-GPLv3
-
-同时支持SAE和普通PHP环境，最低要求PHP5.2.
-请重写所有请求至该文件
-*/
-
 define("ROOT", dirname(__FILE__));
 
-$SaeStorageDomain = "rpup";
-$NonSaeFilesDir = ROOT . "/files";
+include ROOT . "/Qiniu/Client.php";
+\Qiniu\Client::registerAutoloader();
+
+$bucket = "qiniu-up";
+$QiniuBaseUrl = "qiniu-up.qiniudn.com";
+$QiniuAccessKey = '';
+$QiniuSecretKey = '';
+
+$config = array('access_key' => $QiniuAccessKey,'secret_key' => $QiniuSecretKey);
+$sdk = new \Qiniu\Client($config);
 
 ob_start();
-define("InSAE", defined("SAE_ACCESSKEY"));
-
-if(InSAE)
-{
-    $ss = new SaeStorage();
-    $serverInfo["diskUsed"] = $ss->getDomainCapacity($SaeStorageDomain);
-    $serverInfo["fileNum"] = $ss->getFilesNum($SaeStorageDomain);
-}
-else
-{
-    $serverInfo["diskFree"] = disk_free_space(ROOT);
-}
-
-$htmlFileNotExists = <<< HTML
-<section>
-  <div class="alert alert-block alert-error fade in">
-    <header><h3 class="alert-heading">没有找到这个文件</h3></header>
-    <p>
-      可能是：
-    </p>
-    <ul style="list-style-type:none; margin-top: 12px;">
-      <li><i class="icon-tags"></i> 你输入的URL地址有误</li>
-      <li><i class="icon-tags"></i> 这个文件不符合该网盘相关规定而被删除</li>
-      <li><i class="icon-tags"></i> 这个文件体积过大而且较长时间没有被下载</li>
-    </ul>
-  </div>
-</section>
-HTML;
-
-$htmlNeedMoreBit = <<< HTML
-<section>
-  <div class="alert alert-block alert-error fade in">
-    <header><h3 class="alert-heading">该地址对应多个文件</h3></header>
-    <p>
-      你可能需要再多输入几位散列值
-    </p>
-  </div>
-</section>
-HTML;
 
 $htmlFileTableHead = <<< HTML
 <section class='box well'>
@@ -70,80 +27,31 @@ HTML;
 
 switch($_SERVER["REQUEST_METHOD"])
 {
-  case "GET":
-      preg_match("%^/([a-z0-9]{0,32})/?$%", rawurldecode($_SERVER["REQUEST_URI"]), $r);
+    case "POST":
+        if(isset($_FILES["files"]))
+        {
+            echo $htmlFileTableHead;
+            foreach($_FILES["files"]["error"] as $key => $error)
+            {
+                if($error == UPLOAD_ERR_OK)
+                {
+                    $tmpName = $_FILES["files"]["tmp_name"][$key];
+                    $fileName = $_FILES["files"]["name"][$key];
+                    $fileMD5 = md5_file($_FILES["files"]["tmp_name"][$key]);
 
-      if(!empty($r[1]))
-      {
-          $fileMD5 = $r[1];
+                    $params = array('scope' => $bucket,'expires' => 3600);
+                    $body = array('file' => '@' . $tmpName);
+                    list($return, $error) = $sdk->putFile($bucket, $fileMD5, $body, $params);
+                    if ($error !== null) {
+                        $error_arr = json_encode($error);
+                        echo "上传发生错误：{$error_arr['error']}，请稍后再试。";
+                    } else {
+                        $fileSize = ceil($_FILES["files"]["size"][$key] / 1024) . "KB";
+                        $fileType = $_FILES["files"]["type"][$key];
+                        $fileURL = "http://{$QiniuBaseUrl}/{$fileMD5}";
+                        $fileURL = "<a href='{$fileURL}' target='_blank'>{$fileURL}</a>";
 
-          if(InSAE)
-              $files = $ss->getList($SaeStorageDomain, $fileMD5);
-          else
-              $files = glob("{$NonSaeFilesDir}/{$fileMD5}*");
-
-          if(!$files)
-          {
-              echo $htmlFileNotExists;
-          }
-          else if(count($files) > 1)
-          {
-              echo $htmlNeedMoreBit;
-          }
-          else
-          {
-              $file = $files[0];
-              if(InSAE)
-                  $fileName = substr($file, 32);
-              else
-                  $fileName = substr($file, 32 + strlen($NonSaeFilesDir) + 1);
-              $fileName = urlencode($fileName);
-
-              header("Content-Type: application/force-download");
-              header("Content-Disposition: attachment; filename={$fileName}");
-
-              if(InSAE)
-                  echo $ss->read($SaeStorageDomain, $file);
-              else
-                  readfile($file);
-
-              exit();
-          }
-
-      }
-  break;
-  case "POST":
-      if(isset($_FILES["files"]))
-      {
-          echo $htmlFileTableHead;
-          foreach($_FILES["files"]["error"] as $key => $error)
-          {
-              if($error == UPLOAD_ERR_OK)
-              {
-                  $tmpName = $_FILES["files"]["tmp_name"][$key];
-                  $fileName = $_FILES["files"]["name"][$key];
-                  $fileMD5 = md5_file($_FILES["files"]["tmp_name"][$key]);
-                  $objName = $fileMD5 . $fileName;
-
-                  $fileSize = ceil($_FILES["files"]["size"][$key] / 1024) . "KB";
-                  $fileType = $_FILES["files"]["type"][$key];
-                  $fileURL = "http://{$_SERVER["SERVER_NAME"]}/{$fileMD5}";
-                  $fileURL = "<a href='{$fileURL}' target='_blank'>{$fileURL}</a>";
-
-                  if(InSAE)
-                      $files = $ss->getList($SaeStorageDomain, $fileMD5);
-                  else
-                      $files = glob("{$NonSaeFilesDir}/{$fileMD5}*");
-
-                  if(!$files)
-                      if(InSAE)
-                          file_put_contents("saestor://{$SaeStorageDomain}/{$objName}", file_get_contents($tmpName));
-                      else
-                          move_uploaded_file($tmpName, "{$NonSaeFilesDir}/{$objName}");
-                  else
-                      $fileURL .= " (已有其他人上传)";
-
-                  echo <<< HTML
+                        echo <<< HTML
                   <tr>
                     <td>{$fileName}</td>
                     <td>{$fileType}</td>
@@ -151,11 +59,12 @@ switch($_SERVER["REQUEST_METHOD"])
                     <td>{$fileURL}</td>
                   </tr>
 HTML;
-              }
-          }
-          echo $htmlFileTableFooter;
-      }
-  break;
+                    }
+                }
+            }
+            echo $htmlFileTableFooter;
+        }
+        break;
 }
 
 $htmlOut = ob_get_clean();
@@ -163,67 +72,52 @@ $htmlOut = ob_get_clean();
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8">
-    <title>RP UP</title>
-    <link href="http://lib.sinaapp.com/js/bootstrap/latest/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-    <link href="http://lib.sinaapp.com/js/bootstrap/latest/css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css"/>
-  </head>
-  <body style='font-family: "WenQuanYi Micro Hei", "WenQuanYi Zen Hei", "Microsoft YaHei", arial, sans-serif; font-size: 16px;'>
-    <div class="container">
-      <ul class="breadcrumb" style="margin-top: 12px;">
-        <li class="active"><i class="icon-comment"></i> http://<?= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]; ?></li>
-      </ul>
-      <div class="row">
+    <title>7NUP</title>
+    <link href="http://cdn.staticfile.org/twitter-bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+</head>
+<body style='font-family: "WenQuanYi Micro Hei", "WenQuanYi Zen Hei", "Microsoft YaHei", arial, sans-serif; font-size: 16px;margin: 30px auto;'>
+<div class="container">
+    <div class="row">
         <div class="span12">
-          <header class="box well">
-            <header><h2>RP UP</h2></header>
-            <hr />
-            <ul style="list-style-type: none;">
-              <li><i class="icon-ok"></i> 不必注册帐号，不必登录</li>
-              <li><i class="icon-ok"></i> 直链下载，无广告，无需等待</li>
-              <li><i class="icon-ok"></i> 清爽无图界面，无须Flash，同样适合手机访问</li>
-            </ul>
-          </header>
-          <?php echo $htmlOut;?>
-          <section class='box well'>
-            <form id="fileForm" method="post" enctype="multipart/form-data">
-              <input type="file" name="files[]" /><br />
-            </form>
-            <hr />
-            <button id='addFile' class='btn btn-info'><i class="icon-plus icon-white"></i> 添加新的上传框</button>
-            <button id='start' class='btn btn-success'><i class="icon-play icon-white"></i> 开始上传</button>
-          </section>
-          <section class='box well'>
-            <ul style="list-style-type:none;">
-              <li>作者 <a href="http://jyprince.me/" target="_brank">精英王子</a>(<i class="icon-envelope"></i>m@jybox.net)</li>
-              <li>源代码托管于 <a href="https://github.com/jybox/RPUP" target="_brank">Github/RPUP</a> GPLv3</li>
-              <?php if(InSAE):?>
-                <li><a href="http://sae.sina.com.cn" target="_blank"><img src="http://static.sae.sina.com.cn/image/poweredby/117X12px.gif" title="Powered by Sina App Engine"></a></li>
-              <?php endif;?>
-              <li><i class="icon-random"></i> <script src="//static2.jybox.net/my-website/analyzer.js" type="text/javascript"></script></li>
-              <?php if(InSAE):?>
-                <li>已用：<?php echo ceil($serverInfo["diskUsed"] / 1024 / 1024);?>MiB, 共<?php echo $serverInfo["fileNum"];?>个文件</li>
-              <?php else:?>
-                <li>可用空间：<?php echo ceil($serverInfo["diskFree"] / 1024 / 1024);?> MiB</li>
-              <?php endif;?>
-            </ul>
-          </section>
+            <header class="box well">
+                <header><h2>7N UP</h2></header>
+                <hr />
+                <ul style="list-style-type: none;">
+                    <li><i class="icon-ok"></i> 不必注册帐号，不必登录</li>
+                    <li><i class="icon-ok"></i> 直链下载，无广告，无需等待</li>
+                    <li><i class="icon-ok"></i> 七牛CDN全网加速，支持<a href="http://developer.qiniu.com/docs/v6/api/reference/fop/">多媒体在线处理</a></li>
+                    <li><i class="icon-ok"></i> 清爽无图界面，无须Flash，同样适合手机访问</li>
+                </ul>
+            </header>
+            <?php echo $htmlOut;?>
+            <section class='box well'>
+                <form id="fileForm" method="post" enctype="multipart/form-data">
+                    <input type="file" name="files[]" /><br />
+                </form>
+                <hr />
+                <button id='addFile' class='btn btn-info'><i class="icon-plus icon-white"></i> 添加新的上传框</button>
+                <button id='start' class='btn btn-success'><i class="icon-play icon-white"></i> 开始上传</button>
+            </section>
+            <section class='box well'>
+                <ul style="list-style-type:none;">
+                    <li>作者 <a href="http://faceair.net/" target="_brank">faceair</a></li>
+                    <li>源代码托管于 <a href="https://github.com/faceair/7NUP" target="_brank">Github/7NUP</a> GPLv3</li>
+                </ul>
+            </section>
         </div>
-      </div>
     </div>
-    <script type='text/javascript' src='http://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min.js'></script>
-    <script type='text/javascript' src='http://lib.sinaapp.com/js/bootstrap/latest/js/bootstrap.min.js'></script>
-    <!--[if lte IE 8]>
-      <script type='text/javascript' src='//static2.jybox.net/tools/kill-ie6.js'></script>
-    <![endif]-->
-    <script type="text/javascript">
-      $("#addFile").click(function(){
+</div>
+<script type='text/javascript' src='http://cdn.staticfile.org/jquery/2.0.3/jquery.min.js'></script>
+<script type='text/javascript' src='http://cdn.staticfile.org/twitter-bootstrap/3.0.3/js/bootstrap.min.js'></script>
+<script type="text/javascript">
+    $("#addFile").click(function(){
         $("#fileForm").append('<input type="file" name="files[]" /><br />');
-      });
-      $("#start").click(function(){
+    });
+    $("#start").click(function(){
         $("#fileForm").submit();
-      });
-    </script>
-  </body>
+    });
+</script>
+</body>
 </html>
